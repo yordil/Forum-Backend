fcba22f42c2c17cebde6711a7b9d8b9b5b691684
yordanos legesse <yordanoslegesse15@gmail.com>
2024-07-19 11:06:15 +0300

    Cors

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const cors = require("cors");
 const questionRoute = require("./routes/questionRoute");
 const answerRoute = require("./routes/answerRoute");
 // user routes middleware
-app.use(cors());
+app.use(cors({
+	origin: "*",
+}));
 app.use(express.json());
 app.use("/api/users", userRoute);
 
